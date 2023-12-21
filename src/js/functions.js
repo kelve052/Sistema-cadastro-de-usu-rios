1a36ed90de6b1b6ccf9fbb9137b4d7bd0912dc78
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2023-12-21 18:02:12 -0300

    feat (html) refactore (diretorie)

---
+++ src/js/functions.js
@@ -0,0 +1,60 @@
+//funções que manipulão as ações dos botões de troca de ações
+
+function hideAllSections() {
+  const sections = document.querySelectorAll('.div_informacoes');
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+}
+hideAllSections()
+document.getElementById('div_informacoes_home').style.display = 'block';
+
+
+document.getElementById('btnCadastrarUsuario').addEventListener('click', function() {
+  hideAllSections();
+  document.getElementById('div_informacoes_cadastrar').style.display = 'block';
+});
+
+document.getElementById('btnEditarUsuario').addEventListener('click', function() {
+  hideAllSections();
+  document.getElementById('div_informacoes_editar').style.display = 'block';
+});
+
+document.getElementById('btnExcluirUsuario').addEventListener('click', function() {
+  hideAllSections();
+  document.getElementById('div_informacoes_excluir').style.display = 'block';
+});
+
+document.getElementById('btnFazerLogin').addEventListener('click', function() {
+  hideAllSections();
+  document.getElementById('div_informacoes_login').style.display = 'block';
+});
+
+document.getElementById('btnListarUsuarios').addEventListener('click', function() {
+  hideAllSections();
+  document.getElementById('div_informacoes_listar').style.display = 'block';
+});
+
+document.getElementById('btnFazerLogout').addEventListener('click', function() {
+  hideAllSections();
+  document.getElementById('div_informacoes_logout').style.display = 'block';
+});
+
+for (const btnCancelar of document.getElementsByClassName('botao_cancelar')) {
+  btnCancelar.addEventListener('click', function() {
+    hideAllSections();
+    document.getElementById('div_informacoes_home').style.display = 'block';
+  });
+}
+// fim das funções que manipulão as ações dos botões de troca de ações
+
+
+
+
+
+
+
+
+
+
+
